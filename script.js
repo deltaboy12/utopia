1a16df2a8b6ca564b32a0eb97d9445af35e47888
deltaboy12 <delta1phase@gmail.com>
2026-03-15 00:18:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
-function go(){
+function openSite(url){
 
-let input=document.getElementById("search").value
-
-let frame=document.getElementById("frame")
-
-document.getElementById("home").style.display="none"
-
-frame.style.display="block"
-
-if(input.startsWith("http")){
-
-frame.src=input
+window.location.href=url
 
 }
-else if(input.includes(".")){
 
-frame.src="https://"+input
+document.getElementById("search").addEventListener("keydown",function(e){
 
+if(e.key==="Enter"){
+
+let q=this.value
+
+if(q.startsWith("http")){
+window.location.href=q
+}
+else if(q.includes(".")){
+window.location.href="https://"+q
 }
 else{
-
-frame.src="https://duckduckgo.com/?q="+encodeURIComponent(input)
-
+window.location.href="https://duckduckgo.com/?q="+encodeURIComponent(q)
 }
 
 }
 
-function home(){
-
-document.getElementById("frame").style.display="none"
-
-document.getElementById("home").style.display="flex"
-
-}
-
-function cloak(){
-
-let tab=window.open("about:blank","_blank")
-
-let iframe=tab.document.createElement("iframe")
-
-iframe.style.width="100%"
-
-iframe.style.height="100%"
-
-iframe.style.border="none"
-
-iframe.src=window.location.href
-
-tab.document.body.style.margin="0"
-
-tab.document.body.appendChild(iframe)
-
-}
+})
